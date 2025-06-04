aa60db496ef61b8099b12d93ef9a7699528c1bad
avnesh141 <avnesh.kumar.ece21@itbhu.ac.in>
2025-06-04 13:41:48 +0530

    build check

--- server/index.js
+++ server/index.js
@@ -14,14 +14,14 @@ app.use(express.json());
 // API routes
 app.use('/', serverInfoRouter);
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+
   app.use(express.static(path.join(__dirname, '../client/build')));
   
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
-}
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
